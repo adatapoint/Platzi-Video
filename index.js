0558454bd4cb129a8c8f183dffd004733adc938b
Vincent Restrepo <vdrestrepo@geosat.com.co>
2018-11-29 18:38:00 -0500

    Implementada playlist como componente

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 import Media from './src/playlist/components/media'; 
+import data from './src/api.json';
+import Playlist from './src/playlist/components/Playlist';
 // console.log('Hola mundo!')
 const app = document.getElementById('app')
 
 // ReactDOM.render(que-voy-a-renderizar, donde-lo-voy-a-renderizar)
-const holaMundo = <h1>Hola Mundo!</h1>;
-render(<Media type="video" title='¿Qué es responsive Design?' author='Vincent Restrepo' image='/images/covers/responsive.jpg' />, app);
+// const holaMundo = <h1>Hola Mundo!</h1>;
+render(<Playlist data = {data} />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types'
 import './media.css';
 
 class Media extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      author: props.author
-    }
+  state = {
+    author: 'Vince'
   }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     author: props.author
+  //   }
+  // }
   handleClick = (event) => { // Los arrow functions heredan el contexto de su padre, por lo que no es necesario el bind
     // console.log(this.state.image)
-    this.setState({
+    this.setState({ // Así se cambia el estado de un componente.
       author: 'Ricargo Célis',
     })
   }
@@ -21,13 +24,13 @@ class Media extends React.Component {
       <div className="Media" onClick={this.handleClick}>
         <div className="Media-cover">
           <img
-            src={this.props.image}
+            src={this.props.cover}
             alt=""
             width={260}
             height={160}
             />
             <h3 className="Media-title">{this.props.title}</h3>
-            <p className="Media-author">{this.state.author}</p>
+            <p className="Media-author">{this.props.author}</p>
         </div>
       </div>
     )
@@ -35,7 +38,7 @@ class Media extends React.Component {
 }
 
 Media.propTypes = {
-  image: PropTypes.string,
+  cover: PropTypes.string,
   title: PropTypes.string.isRequired, // Se valida que es String, y se valida que sí exista y no venga vacío
   author: PropTypes.string,
   video: PropTypes.oneOf(['video', 'audio']) // Se valida que sea un valor entre los del array
